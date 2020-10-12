439067cecb839df0fd67718a34908136527704fb
ilima12091 <limaignacio12091@gmail.com>
2020-10-12 18:11:07 -0300

    Arreglo los conflictos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -9,15 +9,8 @@ namespace CompAndDel
         static void Main(string[] args)
         {
             PictureProvider p = new PictureProvider();
-<<<<<<< HEAD
-<<<<<<< HEAD
+
             IPicture pic = p.GetPicture("gonzalo.jpg");
-=======
-            IPicture pic = p.GetPicture("PathToImage.jpg");
->>>>>>> 6daa00f... Agrego el ejercicio 1
-=======
-            IPicture pic = p.GetPicture("gonzalo.jpg");
->>>>>>> c000639... Agrego los cambios para el ejericicio 2
 
             FilterGreyscale filterGreyscale = new FilterGreyscale();
             FilterNegative filterNegative = new FilterNegative();
@@ -28,19 +21,9 @@ namespace CompAndDel
             PipeSerial pipeSerialStoreGreyScaleImage = new PipeSerial(filterStoreGreyScaleImage, pipeSerialNegative);
             PipeSerial pipeSerialGreyScale = new PipeSerial(filterGreyscale, pipeSerialStoreGreyScaleImage);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-            pic = pipeSerialGreyScale.Send(pic);
-            Persist(pic, "gonzaloFiltrado.jpg");
-=======
-            pic = pipeNull.Send(pipeSerialNegative.Send(pipeSerialGreyScale.Send(pic)));
-            Persist(pic, "PathToFilteredImage.jpg");
->>>>>>> 6daa00f... Agrego el ejercicio 1
-=======
             pic = pipeSerialGreyScale.Send(pic);
 
             Persist(pic, "gonzaloFiltrado.jpg");
->>>>>>> c000639... Agrego los cambios para el ejericicio 2
         }
 
         public static void Persist(IPicture image, string pathToPersist)
