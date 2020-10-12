69452cf523dca4f44587fd59a28605e568badb08
ilima12091 <limaignacio12091@gmail.com>
2020-10-11 22:05:04 -0300

    Agrego el ejercicio 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -9,7 +9,11 @@ namespace CompAndDel
         static void Main(string[] args)
         {
             PictureProvider p = new PictureProvider();
+<<<<<<< HEAD
             IPicture pic = p.GetPicture("gonzalo.jpg");
+=======
+            IPicture pic = p.GetPicture("PathToImage.jpg");
+>>>>>>> 6daa00f... Agrego el ejercicio 1
 
             FilterGreyscale filterGreyscale = new FilterGreyscale();
             FilterNegative filterNegative = new FilterNegative();
@@ -18,8 +22,13 @@ namespace CompAndDel
             PipeSerial pipeSerialNegative = new PipeSerial(filterNegative, pipeNull);
             PipeSerial pipeSerialGreyScale = new PipeSerial(filterGreyscale, pipeSerialNegative);
 
+<<<<<<< HEAD
             pic = pipeSerialGreyScale.Send(pic);
             Persist(pic, "gonzaloFiltrado.jpg");
+=======
+            pic = pipeNull.Send(pipeSerialNegative.Send(pipeSerialGreyScale.Send(pic)));
+            Persist(pic, "PathToFilteredImage.jpg");
+>>>>>>> 6daa00f... Agrego el ejercicio 1
         }
 
         public static void Persist(IPicture image, string pathToPersist)
