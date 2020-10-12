4fe3b322e09ca0414ef2b302db413f9f7b73051e
ilima12091 <limaignacio12091@gmail.com>
2020-10-11 22:41:00 -0300

    Agrego los cambios para el ejericicio 2

---
+++ src/Library/Filters/FilterStoreImage.cs
@@ -0,0 +1,23 @@
+namespace CompAndDel.Filters
+{
+    public class FilterStoreImage : IFilter
+    {
+        public string PathToPersist { get; set; }
+
+        public FilterStoreImage(string pathToPersist)
+        {
+            this.PathToPersist = pathToPersist;
+        }
+        public IPicture Filter(IPicture image)
+        {
+            this.Persist(image, this.PathToPersist);
+            return image;
+        }
+
+        public void Persist(IPicture image, string pathToPersist)
+        {
+            PictureProvider provider = new PictureProvider();
+            provider.SavePicture(image,pathToPersist);
+        }
+    }
+}
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -10,18 +10,25 @@ namespace CompAndDel
         {
             PictureProvider p = new PictureProvider();
 <<<<<<< HEAD
+<<<<<<< HEAD
             IPicture pic = p.GetPicture("gonzalo.jpg");
 =======
             IPicture pic = p.GetPicture("PathToImage.jpg");
 >>>>>>> 6daa00f... Agrego el ejercicio 1
+=======
+            IPicture pic = p.GetPicture("gonzalo.jpg");
+>>>>>>> c000639... Agrego los cambios para el ejericicio 2
 
             FilterGreyscale filterGreyscale = new FilterGreyscale();
             FilterNegative filterNegative = new FilterNegative();
+            FilterStoreImage filterStoreGreyScaleImage = new FilterStoreImage("GonzaloGreyScaleImageFilter.jpg");
 
             PipeNull pipeNull = new PipeNull();
             PipeSerial pipeSerialNegative = new PipeSerial(filterNegative, pipeNull);
-            PipeSerial pipeSerialGreyScale = new PipeSerial(filterGreyscale, pipeSerialNegative);
+            PipeSerial pipeSerialStoreGreyScaleImage = new PipeSerial(filterStoreGreyScaleImage, pipeSerialNegative);
+            PipeSerial pipeSerialGreyScale = new PipeSerial(filterGreyscale, pipeSerialStoreGreyScaleImage);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
             pic = pipeSerialGreyScale.Send(pic);
             Persist(pic, "gonzaloFiltrado.jpg");
@@ -29,6 +36,11 @@ namespace CompAndDel
             pic = pipeNull.Send(pipeSerialNegative.Send(pipeSerialGreyScale.Send(pic)));
             Persist(pic, "PathToFilteredImage.jpg");
 >>>>>>> 6daa00f... Agrego el ejercicio 1
+=======
+            pic = pipeSerialGreyScale.Send(pic);
+
+            Persist(pic, "gonzaloFiltrado.jpg");
+>>>>>>> c000639... Agrego los cambios para el ejericicio 2
         }
 
         public static void Persist(IPicture image, string pathToPersist)
